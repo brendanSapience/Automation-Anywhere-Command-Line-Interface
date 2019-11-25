e8c119e772217c97140e0fed1dbbf862d2dbdad9
Brendan Sapience <brendan.sapience@gmail.com>
2019-11-25 12:17:00 -0800

    handle weird  html output for IQ Bot API when logged off

--- responses/StdResponses.py
+++ responses/StdResponses.py
@@ -12,10 +12,19 @@ def ProcessBool(myBool):
     else:
         return myBool
 
+def isAnomalousHtmlResponse(text):
+    if("<html" in text and "</html>" in text):
+        return True
+    else:
+        return False
+
 def ProcessStdResponse(res,CsvOutput):
     CsvOutput = ProcessBool(CsvOutput)
-
+    if isAnomalousHtmlResponse(res.text):
+        print("Error: You need to log back in.")
+        exit(1)
     if(res.status_code >= 400):
+
         print("Error Code: "+str(res.status_code))
         try:
             result = json.loads(res.text)
